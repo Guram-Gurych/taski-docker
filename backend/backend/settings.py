38407a1e2e8bdf5924b6b8519b788f6eb1ded8f8
Blednykh Kirill <blednyhkirill@yandex.ru>
2025-04-07 21:46:31 +0300

    com

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['213.176.114.69', 'localhost', 'guram.servebeer.com']
+ALLOWED_HOSTS = ['localhost', 'guram.servebeer.com']
 
 
 # Application definition
